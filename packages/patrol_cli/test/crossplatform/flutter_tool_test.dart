b5c0764f59efdec3e12be930e134bf776c429a7c
Anthony Dade <54687075+AnthonyDade@users.noreply.github.com>
2025-03-28 13:22:12 -0400

    Revert "Update flutter_tool_test.dart"
    This reverts commit 90531efc521cfe285dfab9e9d7554cf7def8c8bd.

--- packages/patrol_cli/test/crossplatform/flutter_tool_test.dart
+++ packages/patrol_cli/test/crossplatform/flutter_tool_test.dart
@@ -87,21 +87,17 @@ void main() {
               .thenAnswer((_) async => process);
 
           // Start the attach process
-          try {
-            await flutterTool.attach(
-              flutterCommand: flutterCommand,
-              deviceId: 'testDeviceId',
-              target: 'target',
-              appId: 'appId',
-              dartDefines: {},
-              openBrowser: false,
-              onQuit: () async {
-                uninstallCalled = true;
-              },
-            );
-          } catch (e) {
-            // Ignore the exception from exit
-          }
+          final future = flutterTool.attach(
+            flutterCommand: flutterCommand,
+            deviceId: 'testDeviceId',
+            target: 'target',
+            appId: 'appId',
+            dartDefines: {},
+            openBrowser: false,
+            onQuit: () async {
+              uninstallCalled = true;
+            },
+          );
 
           // Simulate pressing 'q'
           stdinController.add('q'.codeUnits);
@@ -114,6 +110,12 @@ void main() {
 
           // Verify the uninstall function was called
           expect(uninstallCalled, isTrue);
+
+          // The future should complete with an error due to exit
+          expect(
+            () => future,
+            throwsA(isA<Exception>()),
+          );
         },
       );
     },
